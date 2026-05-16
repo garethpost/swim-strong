726e5357b5e33c09b54a843f2423a13df193990b
garethpost <gareth.post@velloris.io>
2026-05-16 12:56:31 -0600

    app update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SwimFitPro Service Worker
-const CACHE_NAME = 'swimfitpro-v114';
+const CACHE_NAME = 'swimfitpro-v115';
 const CACHE_URLS = [
   './icons/Icon-513.jpeg',
   // index.html intentionally excluded — always fetched fresh from network
